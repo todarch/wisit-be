c3898a4123f1efc579db1964dec5494783ff4e90
selimssevgi <selimssevgi@gmail.com>
2020-04-06 21:46:01 +0200

    feat: create endpoint for simplifying picture resource creation

--- src/main/java/com/todarch/wisitbe/application/picture/PictureManager.java
+++ src/main/java/com/todarch/wisitbe/application/picture/PictureManager.java
@@ -1,8 +1,11 @@
 package com.todarch.wisitbe.application.picture;
 
+import com.todarch.wisitbe.domain.fix.CityRepository;
 import com.todarch.wisitbe.domain.picture.Picture;
 import com.todarch.wisitbe.domain.picture.PictureRepository;
+import com.todarch.wisitbe.rest.picture.PictureResource;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +14,32 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class PictureManager {
 
-  private PictureRepository pictureRepository;
+  private final PictureRepository pictureRepository;
+
+  private final CityRepository cityRepository;
 
   public Picture next() {
     List<Picture> all = pictureRepository.findAll();
     return all.get(ThreadLocalRandom.current().nextInt(all.size()));
+  }
+
+  public Picture newPicture(PictureResource.NewPictureReq picture) {
+    Picture newPic = new Picture();
+    newPic.setUrl(picture.getUrl());
+    newPic.setCityId(findCityId(picture.getCity()));
+    return pictureRepository.save(newPic);
+  }
+
+  private Long findCityId(String givenCity) {
+    return cityRepository.findAll()
+        .stream()
+        .filter(city -> city.getName().equalsIgnoreCase(givenCity))
+        .findFirst()
+        .orElseThrow(() -> new RuntimeException("Not-existing city in system: " + givenCity))
+        .getId();
+  }
+
+  public Optional<Picture> getById(long id) {
+    return pictureRepository.findById(id);
   }
 }
--- src/main/java/com/todarch/wisitbe/rest/picture/PictureResource.java
+++ src/main/java/com/todarch/wisitbe/rest/picture/PictureResource.java
@@ -2,14 +2,22 @@ package com.todarch.wisitbe.rest.picture;
 
 import com.todarch.wisitbe.application.picture.PictureManager;
 import com.todarch.wisitbe.domain.picture.Picture;
+import com.todarch.wisitbe.infrastructure.aspect.InternalOnly;
+import java.util.Objects;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pictures")
+@RequestMapping("api/pictures")
 @AllArgsConstructor
 public class PictureResource {
 
@@ -19,5 +27,29 @@ public class PictureResource {
   public ResponseEntity<Picture> next() {
     Picture next = pictureManager.next();
     return ResponseEntity.ok(next);
+  }
+
+  @GetMapping("/:id")
+  public ResponseEntity<Picture> picById(@PathVariable long id) {
+    Optional<Picture> optionalPic = pictureManager.getById(id);
+    if (optionalPic.isPresent()) {
+      return ResponseEntity.ok(optionalPic.get());
+    }
+
+    return ResponseEntity.notFound().build();
+  }
+
+  @InternalOnly
+  @PostMapping
+  public ResponseEntity<Picture> newPicture(@RequestBody NewPictureReq picture) {
+    Objects.requireNonNull(picture.getUrl(), "pic url cannot be null");
+    Picture createdPic = pictureManager.newPicture(picture);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdPic);
+  }
+
+  @Data
+  public static class NewPictureReq {
+    private String url;
+    private String city;
   }
 }
