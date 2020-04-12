30793a31f8e8327dd0af532c2bae707bb640893a
selimssevgi <selimssevgi@gmail.com>
2020-04-12 22:25:31 +0200

    fix: use spring redis properties

--- src/main/java/com/todarch/wisitbe/infrastructure/redis/RedisConfig.java
+++ src/main/java/com/todarch/wisitbe/infrastructure/redis/RedisConfig.java
@@ -1,16 +1,21 @@
 package com.todarch.wisitbe.infrastructure.redis;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@AllArgsConstructor
 public class RedisConfig {
+
+  private final RedisProperties redisProperties;
 
   @Bean
   LettuceConnectionFactory lettuceConnectionFactory() {
-    return new LettuceConnectionFactory();
+    return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
   }
 
   /**
