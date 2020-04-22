b98f54bbba2f7baae4aacdb3d5e5efea0fec3cf8
selimssevgi <selimssevgi@gmail.com>
2020-04-22 21:46:38 +0200

    refactor: remove deprecated nextFor and answer methods

--- src/main/java/com/todarch/wisitbe/application/question/QuestionManager.java
+++ src/main/java/com/todarch/wisitbe/application/question/QuestionManager.java
@@ -5,28 +5,14 @@ import com.todarch.wisitbe.domain.picture.Picture;
 import com.todarch.wisitbe.domain.picture.PictureRepository;
 import com.todarch.wisitbe.domain.question.Question;
 import com.todarch.wisitbe.domain.question.QuestionRepository;
-import com.todarch.wisitbe.domain.question.UserQuestion;
-import com.todarch.wisitbe.domain.question.UserQuestionRepository;
-import com.todarch.wisitbe.domain.user.User;
-import com.todarch.wisitbe.domain.user.UserRepository;
-import com.todarch.wisitbe.infrastructure.messaging.event.AlmostAllUserQuestionsAskedEvent;
 import com.todarch.wisitbe.infrastructure.messaging.event.QuestionCreatedEvent;
-import com.todarch.wisitbe.infrastructure.messaging.event.UserQuestionAnsweredEvent;
 import com.todarch.wisitbe.infrastructure.messaging.publisher.WisitEventPublisher;
-import com.todarch.wisitbe.infrastructure.rest.errorhandling.InvalidInputException;
 import com.todarch.wisitbe.infrastructure.rest.errorhandling.ResourceNotFoundException;
-import com.todarch.wisitbe.rest.question.AnswerUserQuestion;
 import com.todarch.wisitbe.rest.question.PreparedQuestion;
-import com.todarch.wisitbe.rest.question.PreparedUserQuestion;
-import com.todarch.wisitbe.rest.question.UserQuestionAnswer;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -41,10 +27,6 @@ public class QuestionManager {
 
   private final WisitEventPublisher wisitEventPublisher;
 
-  private final UserQuestionRepository userQuestionRepository;
-
-  private final UserRepository userRepository;
-
   /**
    * Creates a question from a picture.
    */
@@ -53,92 +35,17 @@ public class QuestionManager {
         pictureRepository.findById(pictureId)
         .orElseThrow(() -> new ResourceNotFoundException("Picture not found: " + pictureId));
 
-    doCreateQuestion(foundPicture);
+    Question savedQuestion = doCreateQuestion(foundPicture);
+
+    QuestionCreatedEvent questionCreatedEvent = new QuestionCreatedEvent();
+    questionCreatedEvent.setQuestionId(savedQuestion.getId());
+    wisitEventPublisher.publishEvent(questionCreatedEvent);
   }
 
   private Question doCreateQuestion(Picture picture) {
     Set<Long> choices = locationManager.prepareChoices(picture.getCityId());
     Question newQuestion = new Question(UUID.randomUUID(), picture, choices);
-    Question savedQuestion = questionRepository.save(newQuestion);
-
-    QuestionCreatedEvent questionCreatedEvent = new QuestionCreatedEvent();
-    questionCreatedEvent.setQuestionId(savedQuestion.getId());
-    wisitEventPublisher.publishEvent(questionCreatedEvent);
-
-    return savedQuestion;
-  }
-
-  /**
-   * Finds the next question for the user if exists.
-   */
-  @Deprecated
-  public Optional<PreparedUserQuestion> nextFor(String userId) {
-    User user = userRepository.getById(userId);
-
-    List<UserQuestion> allQuestionsForUser = userQuestionRepository.findAllByUserId(userId);
-
-    int userQuestionCount = allQuestionsForUser.size();
-
-    if (userQuestionCount < 5) {
-      if (user.isEligibleForMoreQuestions()) {
-        AlmostAllUserQuestionsAskedEvent event = new AlmostAllUserQuestionsAskedEvent();
-        event.setUserId(userId);
-        wisitEventPublisher.publishEvent(event);
-      }
-    }
-
-    if (userQuestionCount == 0) {
-      return Optional.empty();
-    }
-
-    int randomIndex = randomIndex(userQuestionCount);
-    UserQuestion userQuestion = allQuestionsForUser.get(randomIndex);
-    return Optional.of(toQuestionWithNoAnswer(userQuestion));
-  }
-
-  private int randomIndex(int bound) {
-    ThreadLocalRandom random = ThreadLocalRandom.current();
-    return random.nextInt(bound);
-  }
-
-  /**
-   * A user answers one of their question.
-   * //TODO: handle the case if user answers a reported/removed user question
-   * @param userId who is answering the question
-   * @param answerUserQuestion which question is being answered
-   * @return the given answer and correct answer
-   */
-  @Deprecated
-  public UserQuestionAnswer answer(@NonNull String userId,
-                                   @NonNull AnswerUserQuestion answerUserQuestion) {
-    String userQuestionId = answerUserQuestion.getUserQuestionId();
-    Objects.requireNonNull(userQuestionId, "UserQuestionId is required");
-
-    UserQuestion userQuestion = userQuestionRepository.findById(userQuestionId)
-        .orElseThrow(() ->
-            new ResourceNotFoundException("UserQuestion not found: " + userQuestionId));
-
-    if (!userQuestion.getUserId().equals(userId)) {
-      throw new InvalidInputException(userQuestionId + " user question is not for user: " + userId);
-    }
-
-    Question question = userQuestion.getQuestion();
-
-    boolean knew = question.isCorrectAnswer(answerUserQuestion.getCityId());
-
-    UserQuestionAnswer userQuestionAnswer = new UserQuestionAnswer();
-    userQuestionAnswer.setUserQuestionId(userQuestion.getId());
-    userQuestionAnswer.setCorrectCity(locationManager.getCityById(question.answerCityId()));
-    userQuestionAnswer.setGivenCity(locationManager.getCityById(answerUserQuestion.getCityId()));
-    userQuestionAnswer.setKnew(knew);
-
-    UserQuestionAnsweredEvent userQuestionAnsweredEvent = new UserQuestionAnsweredEvent();
-    userQuestionAnsweredEvent.setKnew(userQuestionAnswer.isKnew());
-    userQuestionAnsweredEvent.setUserQuestionId(userQuestionAnswer.getUserQuestionId());
-    userQuestionAnsweredEvent.setAnsweredInSeconds(answerUserQuestion.getAnsweredInSeconds());
-    wisitEventPublisher.publishEvent(userQuestionAnsweredEvent);
-
-    return userQuestionAnswer;
+    return questionRepository.save(newQuestion);
   }
 
   public Optional<PreparedQuestion> getById(String questionId) {
@@ -154,16 +61,5 @@ public class QuestionManager {
     preparedQuestion.setChoices(locationManager.toCityNames(question.choices()));
     preparedQuestion.setChoiceCityIds(question.choices());
     return preparedQuestion;
-  }
-
-  private PreparedUserQuestion toQuestionWithNoAnswer(UserQuestion userQuestion) {
-    Question question = userQuestion.getQuestion();
-    PreparedUserQuestion preparedUserQuestion = new PreparedUserQuestion();
-    preparedUserQuestion.setQuestionId(question.getId());
-    preparedUserQuestion.setUserQuestionId(userQuestion.getId());
-    preparedUserQuestion.setPicUrl(question.pictureUrl());
-    preparedUserQuestion.setCreatedAt(question.createdAt());
-    preparedUserQuestion.setChoices(locationManager.toCities(question.choices()));
-    return preparedUserQuestion;
   }
 }
